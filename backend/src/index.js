55c6b4768607e06101b34ea8bc4d911768c69c9a
Sherry <sm718036@gmail.com>
2025-04-05 16:28:07 +0500

    establish mongodb connection and start server if mongodb is connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './db/index.js';
 
 const app = express()
 dotenv.config()
 
 const port = process.env.PORT || 3000
 
-app.listen(port, ()=>{
-  console.log("Server is running on http://localhost:" + port)
+connectDB()
+.then(()=>{
+  app.listen(port, ()=>{
+    console.log("Server is running on http://localhost:" + port)
+  })
+})
+.catch((error)=>{
+  console.log("MongoDB connection failed !!!", error)
 })
